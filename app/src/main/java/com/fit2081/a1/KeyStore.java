691fcf23f35fb59bfdbd3b29994a44bbcbe99411
Vincent <vincent.wesley.liem@gmail.com>
2024-03-20 18:15:19 +0800

    finish category event activity

--- app/src/main/java/com/fit2081/a1/KeyStore.java
+++ app/src/main/java/com/fit2081/a1/KeyStore.java
@@ -1,7 +1,11 @@
 package com.fit2081.a1;
 
 public class KeyStore {
-    public static String FILE_NAME = "USER_CREDENTIALS";
+    public static String FILE_NAME = "SHARED_PREFERENCES";
     public static String KEY_USERNAME = "KEY_USERNAME";
     public static String KEY_PASSWORD = "KEY_PASSWORD";
+    public static String KEY_CATEGORY_ID = "KEY_CATEGORY_ID";
+    public static String KEY_CATEGORY_NAME = "KEY_CATEGORY_NAME";
+    public static String KEY_CATEGORY_EVENT_COUNT = "KEY_CATEGORY_EVENT_COUNT";
+    public static String KEY_IS_CATEGORY_EVENT_ACTIVE = "KEY_IS_CATEGORY_EVENT_ACTIVE";
 }
--- app/src/main/java/com/fit2081/a1/NewEventCategoryActivity.java
+++ app/src/main/java/com/fit2081/a1/NewEventCategoryActivity.java
@@ -1,47 +1,147 @@
 package com.fit2081.a1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.Telephony;
-import android.telephony.SmsMessage;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class NewEventCategoryActivity extends AppCompatActivity {
+    EditText etCategoryId, etCategoryName, etCategoryEventCount;
+    Switch isCategoryEventActive;
+    String[] splitMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event_category);
 
+        etCategoryId = findViewById(R.id.editTextCategoryId);
+        etCategoryName = findViewById(R.id.editTextCategoryName);
+        etCategoryEventCount = findViewById(R.id.editTextCategoryCount);
+        isCategoryEventActive = findViewById(R.id.switchCategoryIsActive);
+
+        etCategoryId.setFocusable(false);
+
         EventCategoryBroadCastReceiver eventCategoryBroadCastReceiver = new EventCategoryBroadCastReceiver();
         registerReceiver(eventCategoryBroadCastReceiver, new IntentFilter(SMSReceiver.SMS_FILTER), RECEIVER_EXPORTED);
+    }
+
+    public void onCreateNewCategoryButtonClick(View view) {
+        String categoryId = etCategoryId.getText().toString().isEmpty() ? generateCategoryId() : etCategoryId.getText().toString();
+        String[] temporaryMessage = new String[3];
+        temporaryMessage[0] = etCategoryName.getText().toString();
+        temporaryMessage[1] = etCategoryEventCount.getText().toString();
+        temporaryMessage[2] = String.valueOf(isCategoryEventActive.isChecked());
+        boolean isValid = checkValidMessage(temporaryMessage);
+
+        if (isValid) {
+            splitMessage = temporaryMessage;
+            saveDataToSharedPreference(categoryId, splitMessage);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Invalid inputs in fields!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void saveDataToSharedPreference(String categoryId, String[] messageDetails) {
+        SharedPreferences sharedPreferences = getSharedPreferences(KeyStore.FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(KeyStore.KEY_CATEGORY_ID, categoryId);
+        editor.putString(KeyStore.KEY_CATEGORY_NAME, messageDetails[0]);
+
+        if (!messageDetails[1].isEmpty()) {
+            int categoryEventCount = Integer.parseInt(messageDetails[1]);
+            editor.putInt(KeyStore.KEY_CATEGORY_EVENT_COUNT, categoryEventCount);
+        }
+
+        if (!messageDetails[2].isEmpty()) {
+            boolean isCategoryEventActive = Boolean.parseBoolean(messageDetails[2]);
+            editor.putBoolean(KeyStore.KEY_IS_CATEGORY_EVENT_ACTIVE, isCategoryEventActive);
+        }
+
+        editor.apply();
+    }
+
+
+    private String generateCategoryId() {
+        String categoryId = "C";
+        for (int i = 0; i < 2; ++i) {
+            categoryId += (char)('A' + (int)(Math.random() * 26));
+        }
+
+        categoryId += "-";
+        for (int i = 0; i < 4; ++i) {
+            categoryId += (char)('0' + (int)(Math.random() * 10));
+        }
+
+        return categoryId;
+    }
+
+    private boolean checkValidMessage(String[] splitMessage) {
+        boolean isValid = true;
+        if (splitMessage.length != 3) {
+            isValid = false;
+        } else {
+            if (splitMessage[0].isEmpty()) {
+                isValid = false;
+            }
+
+            if (!splitMessage[1].isEmpty()) {
+                try {
+                    Integer.parseInt(splitMessage[1]);
+                } catch (Exception e) {
+                    isValid = false;
+                }
+            }
+
+            if (!splitMessage[2].isEmpty() && !splitMessage[2].equalsIgnoreCase("true") && !splitMessage[2].equalsIgnoreCase("false")) {
+                isValid = false;
+            }
+        }
+
+        return isValid;
     }
 
     public class EventCategoryBroadCastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             String message = intent.getStringExtra(SMSReceiver.SMS_MSG_KEY);
-            String[] splitMessage = message.split(";");
-        }
+            String[] identifier = message.split(":");
+            boolean isCorrectIdentifier = true;
 
-        private boolean isMessageValid(String[] splitMessage) {
-            boolean isValid = true;
-            if (splitMessage.length != 3) {
-                isValid = false;
-            } else {
-                try {
-                    Integer.parseInt(splitMessage[2]);
-                } catch (NumberFormatException e) {
-                    isValid = false;
-                }
+            if (!identifier[0].equals("category")) {
+                isCorrectIdentifier = false;
             }
 
-            return isValid;
+            boolean isMessageValid;
+            if (isCorrectIdentifier) {
+                splitMessage = identifier[1].split(";", -1);;
+                isMessageValid = checkValidMessage(splitMessage);
+            } else {
+                isMessageValid = false;
+            }
+
+            if (isMessageValid) {
+                String categoryId = generateCategoryId();
+                etCategoryId.setText(categoryId);
+                etCategoryName.setText(splitMessage[0]);
+                etCategoryEventCount.setText(splitMessage[1]);
+                isCategoryEventActive.setChecked(Boolean.parseBoolean(splitMessage[2]));
+            } else {
+                Toast.makeText(context, "Invalid message format!", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
